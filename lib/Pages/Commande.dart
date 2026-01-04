9fbb7c6b63fffcf4073e739a7b0d0eee09734506
unknown <traoreangefresnel@gmail.com>
2026-01-04 21:31:55 +0000

    ajouter

--- lib/Pages/Commande.dart
+++ lib/Pages/Commande.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gestionnaire_des_commandes_mariam_food/Pages/DetailCommande.dart';
 import 'package:http/http.dart' as http;
+import 'package:lottie/lottie.dart';
 class CommandePage extends StatefulWidget {
   const CommandePage({super.key});
 
@@ -15,23 +17,36 @@ class CommandePage extends StatefulWidget {
 
 class _CommandePageState extends State<CommandePage> {
   var ensemble_de_donnee;
+  var crepes=[];
+  List <String> identifiant=[];
   Future <void> charger_commande() async{
-    final url=Uri.parse("http://10.0.2.2:8000/ensemble_des_commandes");
+    final url=Uri.parse("http://192.168.1.42:8000/ensemble_des_commandes");
     var message=await http.get(url);
       setState(() {
         ensemble_de_donnee=jsonDecode(message.body);
       });
-
-print(ensemble_de_donnee);
+print(ensemble_de_donnee["resultat"].length);
+int i=0;
+print(ensemble_de_donnee["resultat"][1]);
+while(i<ensemble_de_donnee["resultat"].length){
+  print(i);
+  if(ensemble_de_donnee["resultat"][i][3].contains("CRÊPES")){
+    setState(() {
+      crepes.add(ensemble_de_donnee["resultat"][i]);
+    });
+  }else{
+    print("pas ajouté");
+  }
+  setState(() {
+    i++;
+  });
+}
+print(ensemble_de_donnee["resultat"][0]);
+print(crepes);
 
 
   }
-  Future <void> repetition() async{
-    Timer.periodic(
-      Duration(seconds: 5), (timer ) {charger_commande();},
-    );
 
-  }
   @override
   void initState(){
     super.initState();
@@ -45,7 +60,8 @@ print(ensemble_de_donnee);
           child: Stack(
         children: [
         Container(
-            child: Image.asset("assets/images/mariame food.png",height: MediaQuery.of(context).size.height *0.2,)),
+          margin: EdgeInsets.only(top: MediaQuery.of(context).size.height *0.04,left: MediaQuery.of(context).size.width *0.03),
+            child: Image.asset("assets/images/mariame food.png",height: MediaQuery.of(context).size.height *0.17,)),
           Column(
             children: [
               Container(width: MediaQuery.of(context).size.width *1,),
@@ -68,31 +84,57 @@ print(ensemble_de_donnee);
               Container(
                 
                 height: MediaQuery.of(context).size.height *0.77,
-                child: ListView.builder(itemCount:20,itemBuilder: (context, index) => Container(
-                  decoration: BoxDecoration(
-                   ),
-                  child: ListTile(title: Text("TCHEPE POULET | 0150161468",style: TextStyle(color: Color(0xFF632B23),fontFamily: "Poppins"),),trailing: Icon(Icons.subdirectory_arrow_right_outlined,color: Color(0xFF632B23)),
-    leading: Container(
-      width: MediaQuery.of(context).size.width *0.1,
-      child:Icon(FontAwesomeIcons.burger,color: Color(0xFF632B23)) ,),subtitle: Column(children: [
+                child: crepes.isEmpty?
+
+                Container(
+                  child: Column(
+                    children: [
+    Lottie.asset("assets/animations/Frying Pan Breakfast.json",height: MediaQuery.of(context).size.height *0.25,),
+                    Text("Chargement...",style: TextStyle(color: Color(0xFF632B23),fontFamily: "Poppins"))
+    ],)):ListView.builder(itemCount:crepes.length,itemBuilder: (context, index) => Container(
+                  decoration: BoxDecoration(),
+                  child: ListTile(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailcommandePage(crepes:crepes[index])));
+                    },
+                    title: Column(children: [
+    Container(
+      width: MediaQuery.of(context).size.width *1,
+      child: Text("${crepes[index][3]} ",style: TextStyle(color: Color(0xFF632B23),fontFamily: "Poppins"),),
+    ),Container(
+                        width: MediaQuery.of(context).size.width *1,
+                        child: Text("${crepes[index][1]}",style: TextStyle(color: Colors.orange,fontFamily: "Poppins"),)
+                    )],),leading: Container(
+      width: MediaQuery.of(context).size.width *0.12,
+      child:Icon(FontAwesomeIcons.burger,color: Colors.orangeAccent) ,),subtitle: Column(children: [
                     Container(
                       width: MediaQuery.of(context).size.width *1, 
                         child:
-                    Text("Prix : 1000 FCFA",style: TextStyle(color: Color(0xFF632B23),fontFamily: "Poppins"),)
+    Row(children: [
+      Text("Prix : ",style: TextStyle(color: Color(0xFF632B23),fontFamily: "Poppins"),)
+
+      ,Text("${crepes[index][6]} FCFA",style: TextStyle(color: Colors.green,fontFamily: "Poppins"),)
+
+    ],)
                     ),
                     Container(
                         width: MediaQuery.of(context).size.width *1,
                         color: Colors.white,
                         child:
-                        Text("Quantité : x2 ",style: TextStyle(color: Color(0xFF632B23),fontFamily: "Poppins"),)
+    Row(
+      children: [
+        Text("Quantité : ",style: TextStyle(color: Color(0xFF632B23),fontFamily: "Poppins"),)
+,Text("x${crepes[index][2]}",style: TextStyle(color: Colors.green,fontFamily: "Poppins"),)
+
+        ],)
                     ),
                     Container(
                         width: MediaQuery.of(context).size.width *1,
                         color: Colors.white,
                         child:
-                        Text("2025-01-12 22:30:22",style: TextStyle(color: Color(0xFF632B23),fontFamily: "Poppins"),)
+                        Text("${crepes[index][4]}",style: TextStyle(color: Colors.orange,fontFamily: "Poppins"),)
                     )
-                  ],),),
+                  ],),trailing: Icon(Icons.subdirectory_arrow_right_outlined,color: Color(0xFF632B23)),),
                 ),)
                 ,)
 
--- lib/Pages/DetailCommande.dart
+++ lib/Pages/DetailCommande.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher_android/url_launcher_android.dart';
 
 class DetailcommandePage extends StatefulWidget {
-  const DetailcommandePage({super.key});
-
+  DetailcommandePage({super.key, required this.crepes});
+var crepes;
   @override
   State<DetailcommandePage> createState() => _DetailcommandePageState();
 }
 
 class _DetailcommandePageState extends State<DetailcommandePage> {
+Future<void> lancer_whatsapp() async{
+  final url=Uri.parse("https://wa.me/225${widget.crepes[1]}");
+  var whatsapp=launchUrl(url,mode: LaunchMode.externalApplication);
+}
+Future<void> lancer_telephone() async{
+  final url=Uri(scheme: 'tel',path:"225${widget.crepes[1]}" );
+  var whatsapp=launchUrl(url,mode: LaunchMode.externalApplication);
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,50 +49,74 @@ class _DetailcommandePageState extends State<DetailcommandePage> {
               Container(
                 margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
                 height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child: Text("Identifiant : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
+                width: MediaQuery.of(context).size.width *1,child: Row(children: [
+                Text("Identifiant : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
+                Text("${widget.crepes[0]}",style: TextStyle(fontFamily: "Poppins",color: Colors.orange),),
+
+              ],),
               ),
               Container(
                 margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
                 height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child:Text("Nom complet : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
+                width: MediaQuery.of(context).size.width *1,child:Row(
+                children: [
+                  Text("Nom complet : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),)
+    ,Text("${widget.crepes[5]}",style: TextStyle(fontFamily: "Poppins",color: Colors.green),),
+
+    ],),
               ),
               Container(
                 margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
                 height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child: Text("Nom complet : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
-              ),
+                width: MediaQuery.of(context).size.width *1,child:Row(
+    children: [
+    Text("Numero : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),)
+    ,Text("${widget.crepes[1]}",style: TextStyle(fontFamily: "Poppins",color: Colors.green),),
+
+    ],)      ),
+
               Container(
                 margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
                 height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child:Text("Numero : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
-              ),
+                width: MediaQuery.of(context).size.width *1,child:Row(
+    children: [
+    Text("Date : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),)
+    ,Text("${widget.crepes[4]}",style: TextStyle(fontFamily: "Poppins",color: Colors.orange),),
+
+    ],) ),
               Container(
                 margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
                 height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child:Text("Date : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
-              ),
+                width: MediaQuery.of(context).size.width *1,child:Row(
+    children: [
+    Text("Commande : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),)
+    ,Text("${widget.crepes[3]}",style: TextStyle(fontFamily: "Poppins",color: Colors.orange),),
+
+    ],)),
               Container(
                 margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
                 height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child:Text("Date : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
-              ),
+                width: MediaQuery.of(context).size.width *1,child:Row(
+    children: [
+    Text("Prix : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),)
+    ,Text("${widget.crepes[6]} FCFA",style: TextStyle(fontFamily: "Poppins",color: Colors.orange),),
+
+    ],)),
               Container(
                 margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
                 height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child:Text("Commande : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
-                height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child:Text("Prix : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.06),
-                height: MediaQuery.of(context).size.height *0.035,
-                width: MediaQuery.of(context).size.width *1,child:Text("Quantité : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),),
-              ),
+                width: MediaQuery.of(context).size.width *1,child:Row(
+    children: [
+    Text("Quantite : ",style: TextStyle(fontFamily: "Poppins",color: Color(0xFF632B23)),)
+    ,Text("x${widget.crepes[2]}",style: TextStyle(fontFamily: "Poppins",color: Colors.orange),),
+
+    ],)),
               SizedBox(height: MediaQuery.of(context).size.height *0.03,),
-              Container(
+              GestureDetector(
+                onTap: (){
+                  lancer_whatsapp();
+                },
+                child: Container(
                 height: MediaQuery.of(context).size.height *0.1,
                 width: MediaQuery.of(context).size.width *0.6,
                 decoration: BoxDecoration(
@@ -95,9 +129,11 @@ class _DetailcommandePageState extends State<DetailcommandePage> {
                     Icon(FontAwesomeIcons.whatsapp,color: Colors.white,size: MediaQuery.of(context).size.width *0.12,),
                     Text("WHATSAPP",style: TextStyle(fontFamily: "Poppins",fontSize: MediaQuery.of(context).size.width *0.05,color: Colors.white),)
                   ],),
-              ),
+              ),),
               SizedBox(height: MediaQuery.of(context).size.height *0.03,),
-
+GestureDetector(
+  onTap: (){lancer_telephone();},
+  child:
               Container(
                 height: MediaQuery.of(context).size.height *0.1,
                 width: MediaQuery.of(context).size.width *0.6,
@@ -111,7 +147,7 @@ class _DetailcommandePageState extends State<DetailcommandePage> {
                     Icon(FontAwesomeIcons.phone,color: Colors.white,size: MediaQuery.of(context).size.width *0.1,),
                     Text("TELEPHONE",style: TextStyle(fontFamily: "Poppins",fontSize: MediaQuery.of(context).size.width *0.05,color: Colors.white),)
                   ],),
-              ),
+              ),),
               SizedBox(height: MediaQuery.of(context).size.height *0.03,),
 
 
